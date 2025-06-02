6283dde6b04da26c2ffac622a78f516b8243eb0b
Dante <dkirsman@gmail.com>
2025-06-01 20:52:21 -0600

    refactor(motion): remove motion from button as I can use simple css

--- components/button.tsx
+++ components/button.tsx
@@ -1,20 +1,17 @@
-import { motion } from "motion/react";
-import { ReactNode, MouseEventHandler } from "react"
+import { ReactNode, MouseEventHandler } from "react";
 
 interface ButtonProps {
-  children: ReactNode
-  onClick?: MouseEventHandler<HTMLDivElement>
+  children: ReactNode;
+  onClick?: MouseEventHandler<HTMLDivElement>;
 }
 
 export default function Button({ children, onClick }: ButtonProps) {
   return (
-    <motion.div
+    <div
       onClick={onClick}
-      className="flex items-center bg-button transition-colors dark:hover:bg-button/60 hover:bg-[#ccc] w-fit gap-1 py-2 px-3 rounded-2xl cursor-pointer"
-      whileHover={{ scale: 1.01, y: -1 }}
-      whileTap={{ scale: 0.95 }}
+      className="flex items-center bg-button transition-all dark:hover:bg-button/60 hover:bg-[#ccc] w-fit gap-1 py-2 px-3 rounded-2xl cursor-pointer hover:scale-[1.01] hover:-translate-y-[1px] active:scale-[0.95]"
     >
       {children}
-    </motion.div>
+    </div>
   );
 }
