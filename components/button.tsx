3d20a9c7e8c691a2d4f788167a4077d260dd6a33
Dante <dkirsman@gmail.com>
2025-06-01 18:51:06 -0600

    feat(button animate): animate the buttons slightly

--- components/button.tsx
+++ components/button.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import { ReactNode, MouseEventHandler } from "react"
 
 interface ButtonProps {
@@ -7,11 +8,13 @@ interface ButtonProps {
 
 export default function Button({ children, onClick }: ButtonProps) {
   return (
-    <div
+    <motion.div
       onClick={onClick}
       className="flex items-center bg-button transition-colors dark:hover:bg-button/60 hover:bg-[#ccc] w-fit gap-1 py-2 px-3 rounded-2xl cursor-pointer"
+      whileHover={{ scale: 1.01, y: -1 }}
+      whileTap={{ scale: 0.95 }}
     >
       {children}
-    </div>
-  )
+    </motion.div>
+  );
 }
